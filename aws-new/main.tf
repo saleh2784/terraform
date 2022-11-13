54f357f24fa4c7d38575189baadffb637d341520
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-13 17:37:35 +0200

    update lb

--- aws-new/main.tf
+++ aws-new/main.tf
@@ -110,18 +110,3 @@ resource "aws_lb" "aws_lb_tf" {
 output "ELB public DNS" {
   value = aws_lb.aws_lb_tf.public_dns
 }
-
-# # Network Load Balancer
-# resource "aws_lb" "test" {
-#   name               = "test-lb-tf"
-#   internal           = false
-#   load_balancer_type = "network"
-#   subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-#   enable_deletion_protection = true
-
-#   tags = {
-#     Environment = "production"
-#   }
-# }
-
