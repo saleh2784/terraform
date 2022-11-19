7eb2b959b822ee67e809e85f2fb5fd6d9a4507f1
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-19 13:15:43 +0200

    output.tf

--- aws-new/main.tf
+++ aws-new/main.tf
@@ -1,7 +1,7 @@
 # https://github.com/yanivomc/seminars/blob/terraform/terraform/part-2/final-exam.md#L42
 
 provider "aws" {
+  region = var.aws_region
   access_key = "ddddddddddd"
   secret_key = "rrrrrrrrrrr"
-  region = var.aws_region
 }
--- aws-new/output.tf
+++ aws-new/output.tf
@@ -3,3 +3,6 @@
 output "ELB public DNS" {
   value = aws_lb.aws_alb_tf.public_dns
 }
+output "ec2_global_ips" {
+  value = "${aws_instance.myInstance.*.public_ip}"
+}
