16f36392732817f9e40dbf141945753daad1a3e8
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-19 13:11:53 +0200

    updated sg.tf

--- aws-new/sg.tf
+++ aws-new/sg.tf
@@ -3,35 +3,36 @@
 resource "aws_security_group" "TF-SG" {
   name        = var.security_group
   description = "Allow inbound & outbound firewall traffic"
+  vpc_id = aws_vpc.main.id
+
 
   ingress {
+    description = "http"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
- # outbound from nginx server
+ # outbound
  #Egress means traffic that’s leaving from inside the private network out to the public internet.
 
-  egress {
-    from_port   = 80 # should be any port NTC
-    to_port     = 80 # should be any port NTC
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0 
     to_port     = 0 
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags= {
