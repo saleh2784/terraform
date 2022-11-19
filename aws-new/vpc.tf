c8e18e520842b27c66781180af58f4e24479bd26
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-19 21:34:05 +0200

    create subnet

--- aws-new/vpc.tf
+++ aws-new/vpc.tf
@@ -1,9 +1,20 @@
 # create vpc
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
+resource "aws_vpc" "lab-vpc" {
+  cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "main"
+    Name = "lab-vpc"
+  }
+}
+
+# create subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "lab-subnet"
   }
 }
