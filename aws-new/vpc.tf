282771060fe58a4103e4ec1f171086315d2e572a
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-19 21:47:56 +0200

    subnet-2

--- aws-new/vpc.tf
+++ aws-new/vpc.tf
@@ -15,6 +15,16 @@ resource "aws_subnet" "subnet-1" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "lab-subnet"
+    Name = "lab-subnet-1"
   }
 }
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.lab-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "lab-subnet-2"
+  }
+}
+
