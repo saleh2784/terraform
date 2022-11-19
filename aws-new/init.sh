1cb088c3c536e702e3ea80780fc247c9d7608c41
saleh2784 <60964062+saleh2784@users.noreply.github.com>
2022-11-19 12:10:17 +0200

    init script

---
+++ aws-new/init.sh
@@ -0,0 +1,6 @@
+sudo su 
+yum check-update
+yum install -y httpd
+chkconfig httpd on
+echo "<h1><b1>My new devops lab</b1></h1>" > /var/www/html/index.html
+service httpd start
---
+++ aws-new/key.tf
@@ -0,0 +1,5 @@
+resource "aws_key_pair" "lab-key" {
+    key_name = "lab-key"
+    
+  
+}
